e3109e1da2272860298e9cd80453959b3d3913af
Sonal Mittal <sonalmittal140103@gmail.com>
2025-02-27 15:11:10 +0530

    Navbar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,120 @@
+import basket_icon from './basket.png'
+import logo from './logo.png'
+import header_img from './header.jpg'
+import search_icon from './search.jpg'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+
+import food_1 from './food_1.png'
+
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+} 
+
+
+
+export const menu_list = [
+    {
+        menu_name : "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Desserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwitch",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cakes",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Indian delight",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }
+]
+
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "chiken Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "3",
+        name: "masala Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "4",
+        name: "Greennnn Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "5",
+        name: "hehe Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "6",
+        name: " bye Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    },
+    {
+        _id: "7",
+        name: "Maze ka Salad",
+        image: food_1,
+        price: 12,
+        description: "Food provides essential nutrients for overall health and well-being.",
+        category: "Salad"
+    }
+]

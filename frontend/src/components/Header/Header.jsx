5945d6ac68f0703993034f8dddbd3e8cc6c76ac7
Sonal Mittal <sonalmittal140103@gmail.com>
2025-03-02 23:24:26 +0530

    Header Section

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil voluptates dicta voluptate, sunt incidunt eum a possimus corrupti corporis quibusdam ullam quo molestias? Quisquam officia voluptatibus voluptatum, quibusdam placeat in.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
